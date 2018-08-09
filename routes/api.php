bc30d0a7f9aa5c0c1c3d712f9dd2f7b61e1e2a45
juan vargas <juanvargasva.seo@gmail.com>
2018-08-09 01:36:31 -0500

    sadasd

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,7 @@ Route::middleware(['jwt.auth'])->group(function(){
 Route::post('login','AuthenticateController@authenticate');
 
 Route::get('records',function(){
-	$records = App\Record::orderBy('created_at', 'DESC')->get();
+	$records = App\Record::orderBy('created_at', 'ASC')->get();
 	return json_encode([
 		'records'=>$records,//ARRAY DE REGISTROS
 		'count'=>$records->count(),// CANTIDAD DE REGISTROS
