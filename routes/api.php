d8dc2894912e3a6ba16e9736f920ed8cd67cbd8d
juan vargas <juanvargasva.seo@gmail.com>
2018-08-09 18:15:16 -0500

    asdas

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::get('rec',function(Request $request){
 	$record->humidity = $request->input('humidity');
 	$record->co2 = rand(10, 90);
 	$record->save();
-	return $record->created_at; 
+	return "correcto";
 });
 
 Route::get('records',function(){
